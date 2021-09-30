489a36ce7c46c8ff66c86cecc66a431a042d6cc2
Ben Clarke <benthedev@outlook.com>
2021-09-30 18:06:29 +0100

    Update to hero.jsx
    Removed twitch icon and replaced with Youtube icon for my channel and added Youtube icon for BenTheDev channel
    Also removed Twitch from streamer in description

--- portfolio/src/components/hero/hero.jsx
+++ portfolio/src/components/hero/hero.jsx
@@ -10,14 +10,15 @@ export default class Home extends Component {
             <div className="s-hero__content-about">
                 <h1>I'm Ben Clarke.</h1>
                 <h3>
-                I'm a Irish based <span>software engineer</span>, <span>football coach</span> and <span>part-time twitch streamer</span>. I love creating efficient things that people enjoy using. Let's <a className="smoothscroll" href="#about">start scrolling </a> 
+                I'm a Irish based <span>software engineer</span>, <span>football coach</span> and <span>part-time streamer</span>. I love creating efficient things that people enjoy using. Let's <a className="smoothscroll" href="#about">start scrolling </a> 
                 and learn more <a className="smoothscroll" href="#about">about me</a>.
                 </h3>
                 <div className="s-hero__content-social">
                 <a href="https://www.linkedin.com/in/ben-clarke-ie/"><i className="fab fa-linkedin" aria-hidden="true" /></a>
                 <a href="https://twitter.com/benjithedev"><i className="fab fa-twitter" aria-hidden="true" /></a>
                 <a href="https://instagram.com/benthedev"><i className="fab fa-instagram" aria-hidden="true" /></a>
-                <a href="https://twitch.tv/benthedev"><i className="fab fa-twitch" aria-hidden="true" /></a>
+                <a href="https://www.youtube.com/channel/UCMTVI1SRqBO75-xojcxd4mA"><i className="fab fa-youtube" aria-hidden="true" /></a>
+                <a href="https://www.youtube.com/channel/UCNdS-pvLi8bfWc9j2FnBFWw"><i className="fab fa-youtube" aria-hidden="true" /></a>
                 </div>
             </div> {/* end s-hero__content-about */}
             </div>
